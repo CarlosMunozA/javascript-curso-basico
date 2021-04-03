f92c74d6bde2d4028b7be5946949c7c0d7b35620
Carlosl <munozcarlos2i@gmail.com>
2021-04-03 14:54:38 +0200

    curso javascript terminado

---
+++ app.js
@@ -0,0 +1,121 @@
+//tipos de datos
+
+
+/*"Hello World" //string
+'Hello World' //string
+'I am'
+"I'am"
+
+.anchor
+
+1000
+
+true
+false
+
+['joe', 'ryan','martha'];
+[1,3,4,5123,344]
+[true,false,true,true,false]
+
+//object
+persona{'ryan,70.4,20,true'};
+
+{
+    "username": 'ryan',
+    "score": '70.4,
+    "hours":15,
+    "proffesional":true
+    "friends":['','','','']
+}
+
+{
+    "username": 'peiro',
+    "score": '15,
+    "hours":2,
+    "proffesional":false,
+    "friends":['','','','']
+}
+*/
+/*console.log({
+    "username": 'peiro',
+    "score": 15,
+    "hours":2,
+    "proffesional":false,
+    "friends":['','','','']
+});
+
+var nameUser ='Jhon';
+let lastname ='Carter';
+nameUser='Pepe';
+//una constante no cambia nunca
+const PI = 3.1415;
+console.log(nameUser+" "+ lastname+ PI);
+
+
+let numberOne = 100;
+let numberTwo = 12;
+
+let result = numberOne != numberTwo;
+console.log(result);
+
+
+let passwordDB = 'pepe123'
+
+let input = 'pepe123'
+let result = input == passwordDB;
+
+if (result === true){
+    console.log('Login Correcto')
+}
+else{
+    console.log('Contraseña incorrecto')
+}
+
+let score = 70;
+
+if (score > 30){
+    console.log('You need to practice more');
+}
+else if (score > 15){
+    console.log('Estas mejorando');
+}
+else{
+    console.log('You need to follow this tutorial')
+}
+
+
+let typeCard = 'Debit Card';
+
+switch(typeCard){
+    case 'Debit Card':
+        console.log('This is a debit card');
+        break;
+    case 'Credit Card':
+        console.log('This is a credit card');
+        break;
+    default:
+        console.log('No Card')
+}
+
+
+let msj='Hola Mundo';
+let count=50 
+while(count>0){
+    console.log('Hello World');
+    console.log(count);
+    count = count - 1;
+}
+
+
+let array = ['Peiro','Juan','Fede'];
+for(let i = 0; i < array.length ;i++){
+    console.log('yeey '+i + array[i]);
+}
+*/
+
+function greeting (name){
+    console.log('Hello');
+    console.log(name);
+
+}
+greeting('Peiro')
